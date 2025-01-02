8782417aed497af395249acd7709acaf0ea634bc
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-01-01 22:42:02 -0400

    implement error handling and user agent rotation in carrega_site function

--- document_loaders.py
+++ document_loaders.py
@@ -1,3 +1,6 @@
+import os
+from time import sleep
+import streamlit as st
 from langchain_community.document_loaders import (
     WebBaseLoader,
     YoutubeLoader,
@@ -5,13 +8,26 @@ from langchain_community.document_loaders import (
     PyPDFLoader,
     TextLoader
 )
+from fake_useragent import UserAgent
 
 
 # url_site = 'http://2025.pythonbrasil.org.br/'
 def carrega_site(url_site):
-    loader = WebBaseLoader(url_site, verify_ssl=False)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    documento = ''
+    for i in range(5):     
+        try:
+            os.environ['USER_AGENT'] = UserAgent().random
+            loader = WebBaseLoader(url_site, verify_ssl=False, raise_for_status=True)
+            lista_documentos = loader.load()
+            documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+            break
+        except:
+            print(f'Erro ao carregar o site {i+1}')
+            sleep(3)
+    
+    if documento == '':
+        st.error('Não foi possível carregar o site')
+        st.stop()
     return documento
 
 
